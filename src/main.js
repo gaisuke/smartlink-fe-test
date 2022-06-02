2aa89b2cda16bbe7e9caad25e756fc137fba9b69
gaisuke <danimunif@cs.unej.ac.id>
2022-06-02 22:15:50 +0700

    version 2.0 of consume, still failed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import { createApp } from 'vue'
+import Vue
+// { createApp }
+from 'vue'
 import App from './App.vue'
-import store from "./store"
+import store from "./store/index"
 
-createApp(App)
-.use(store)
-.mount('#app')
+// createApp(App)
+// .use(store)
+// .mount('#app')
+
+new Vue({
+    render: (h) => h(App),
+    store,
+}).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,31 @@
-//import the createStore object from Vuex
-import { createStore } from 'vuex'
-// Import axios to make HTTP requests
-import axios from "axios"
-export default createStore({
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from "axios";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
     state: {
         banks: [],
-        inquiries: []
     },
     getters: {
-        getBanks: (state) => state.banks,
-        getInquiries: (state) => state.inquiries
-    },
-    actions: {
-        async fetchBanks({ commit }) {
-            try {
-                const data = await axios.get('https://boss.smartlink.id/salary/bank')
-                commit('SET_BANKS', data.data)
-            } catch (error) {
-                alert(error)
-                console.log(error)
-            }
-        },
-        async fetchInquiries({ commit }) {
-            try {
-                const data = await axios.get('https://boss.smartlink.id/salary/inquiry')
-                commit('SET_INQUIRIES', data.data)
-            } catch (error) {
-                alert(error)
-                console.log(error)
-            }
+        banks: state => {
+            return state.banks;
         }
     },
     mutations: {
-        SET_BANKS(state, banks) {
+        SET_ITEMS (state, banks) {
             state.banks = banks
-        },
-        SET_INQUIRIES(state, inquiries) {
-            state.inquiries = inquiries
-        },
+        }
+    },
+    action: {
+        async loadBanks ({ commit }) {
+            try {
+                const response = await axios.get('https://boss.smartlink.id/salary/bank');
+                commit('SET_ITEMS', response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
     }
-})
-/** we have just created a boiler plate for our vuex store module**/
+}) 
