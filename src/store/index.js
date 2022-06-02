711bace9e4bb8b6a2443b627304194902bcd8999
gaisuke <danimunif@cs.unej.ac.id>
2022-06-02 21:09:53 +0700

    setup store for using vuex

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+//import the createStore object from Vuex
+import { createStore } from 'vuex'
+// Import axios to make HTTP requests
+// eslint-disable-next-line no-unused-vars
+import axios from "axios"
+export default createStore({
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {}
+})
+/** we have just created a boiler plate for our vuex store module**/
